463e7a3a817e8a916638eb47964cb578920ad6fa
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-09-25 11:56:58 +0800

    Some Updates

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -1,6 +1,13 @@
-'use client'
+"use client";
 
-const Heading = () => {
+import React from "react";
+
+interface HeadingProps {
+  title: string;
+  subtitle?: string;
+  center?: boolean;
+}
+const Heading: React.FC<HeadingProps> = ({ title, subtitle, center }) => {
   return <div>Hello heading</div>;
 };
 
