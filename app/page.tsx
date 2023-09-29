9ac106d6136c1d4046093c6a191a28d7de5ea581
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-09-29 12:08:38 +0800

    chanegs PLes work

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Container from "@/app/components/Container";
 import ListingCard from "@/app/components/listings/ListingCard";
 import EmptyState from "@/app/components/EmptyState";
